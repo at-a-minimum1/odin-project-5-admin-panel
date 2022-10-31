cc22b6ff8a9157bec43c498b759853131820a18c
Timothy Beler <timothybeler4@gmail.com>
2022-10-31 03:43:27 -0400

    Added custom component for the issue cards and added styles for the site.

---
+++ app.js
@@ -0,0 +1,9 @@
+// Refer back to this video to make the rest of the component https://youtu.be/QWvK5_5hL7I 
+
+class Card extends HTMLElement {
+	constructor() {
+		super();
+	}
+}
+
+customElements.define("card", Card);
