a1fbca8c2b2fa3c04870ec08ca5e96c5471cea2d
Timothy Beler <timothybeler4@gmail.com>
2022-11-06 01:32:03 -0500

    Updated the Javascript and CSS to make the panel class and most of the sort functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Refer back to this video to make the rest of the component https://youtu.be/QWvK5_5hL7I 
+// Refer back to this video to make the rest of the component https://youtu.be/QWvK5_5hL7I
 
 class Card extends HTMLElement {
 	constructor() {
@@ -7,3 +7,43 @@ class Card extends HTMLElement {
 }
 
 customElements.define("card", Card);
+
+function showOptions(num) {
+	if (num == "0") {
+		document.getElementById("assigneeDropdown").classList.toggle("hide");
+	}
+	if (num == "1") {
+		document.getElementById("reporterDropdown").classList.toggle("hide");
+	}
+	if (num == "2") {
+		document.getElementById("statusDropdown").classList.toggle("hide");
+	}
+	if (num == "3") {
+		document.getElementById("typeDropdown").classList.toggle("hide");
+	}
+	if (num == "4") {
+		document.getElementById("statusCategoryDropdown").classList.toggle("hide");
+	}
+	if (num == "5") {
+		document.getElementById("sortPanel").classList.toggle("hide");
+	}
+}
+document.getElementById("collapse-filter").onclick = (e) => {
+	e.preventDefault();
+};
+
+
+
+// window.onclick = function (event) {
+// 	if (!event.target.matches(".dropbtn")) {
+// 		var dropdowns = document.getElementsByClassName("dropdown-content");
+
+// 		var i;
+// 		for (i = 0; i < dropdowns.length; i++) {
+// 			var openDropdown = dropdowns[i];
+// 			if (openDropdown.classList.contains("show")) {
+// 				openDropdown.classList.remove("show");
+// 			}
+// 		}
+// 	}
+// };
